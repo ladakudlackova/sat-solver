60ab425d74274e673ed1e13d311b24165bdbe4b5
ladakudlackova <ladaGkudlackova@gmail.com>
2020-04-04 07:13:04 +0200

    Builds folder
    Translation as main method.
    cnf2dimacs: jar + script

---
+++ bin/builds/1_cnf2dimacs/cnf2dimacs.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar cnf2dimacs.jar "$@"
--- src/tseitin_to_dimacs/Translation.java
+++ src/tseitin_to_dimacs/Translation.java
@@ -4,8 +4,6 @@ import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
-import java.util.List;
-
 import simple_nnf_tree.DerivationTree;
 import tseitin.CNFDefinition.implicationType;
 import utils.IOUtils;
@@ -13,51 +11,76 @@ import tseitin.Encoding;
 
 public class Translation {
 
-	private static final String NOT_NNF = "Input string is not nnf.";	
-	private static boolean valid = true;	
+	private static final String NOT_NNF = "Input string is not nnf.";
+	private static boolean valid = true;
 	private static Encoding tseitinEncoding;
 
-	public static void formula2cnf(boolean bothImplications){
+	// TODO: code properly
+	public static void main(String[] args) {
+		Boolean bothImplications = null;
+		if (args.length > 0) {
+			String arg0 = args[args.length-1].toLowerCase();
+			if (arg0.equals("true") || arg0.equals("1"))
+				bothImplications = true;
+			else 
+				bothImplications = false;
+			switch (args.length) {
+			case 1:
+				formula2cnf(bothImplications);
+				return;
+			case 2:
+				formula2cnf(bothImplications, args[1]);
+				return;
+			case 3:
+				formula2cnf(bothImplications, args[1], args[2]);
+				return;
+			default:
+				break;
+			}
+		}
+		System.out.println("Invalid count of parameters.");
+	}
+
+	public static void formula2cnf(boolean bothImplications) {
 		translateAndPrint(bothImplications, new InputStreamReader(System.in), System.out);
 	}
-	
-	public static void formula2cnf(boolean bothImplications, String inputFileName){
+
+	public static void formula2cnf(boolean bothImplications, String inputFileName) {
 		Reader r = IOUtils.createReader(inputFileName);
 		translateAndPrint(bothImplications, r, System.out);
 	}
-	
-	public static void formula2cnf(boolean bothImplications, String inputFileName, String outputFileName){
+
+	public static void formula2cnf(boolean bothImplications, String inputFileName, String outputFileName) {
 		Reader r = IOUtils.createReader(inputFileName);
 		FileOutputStream fos = IOUtils.createOutputFile(outputFileName);
 		translateAndPrint(bothImplications, r, fos);
 	}
-	
-	public static DimacsCNF formula2dimacsCNF(boolean bothImplications, String inputFileName){
+
+	public static DimacsCNF formula2dimacsCNF(boolean bothImplications, String inputFileName) {
 		Reader r = IOUtils.createReader(inputFileName);
 		return translate(r, bothImplications);
 	}
-	
-	
+
 	private static void translateAndPrint(boolean bothImplications, Reader r, OutputStream os) {
-		valid = (r!=null && os!=null);
+		valid = (r != null && os != null);
 		if (valid) {
 			DimacsCNF dimacsCNF = translate(r, bothImplications);
-			String cnf=dimacsCNF.toString();
+			String cnf = dimacsCNF.toString();
 			if (valid)
 				IOUtils.print(os, cnf, valid);
 		}
 	}
-	
+
 	private static DimacsCNF translate(Reader r, boolean bothImplications) {
-		
-		implicationType implType = bothImplications? implicationType.EQUIVALENCE : implicationType.LEFT_TO_RIGHT;
+
+		implicationType implType = bothImplications ? implicationType.EQUIVALENCE : implicationType.LEFT_TO_RIGHT;
 		DerivationTree tree = new DerivationTree(r);
-		valid=tree.validNNF();
+		valid = tree.validNNF();
 		if (valid) {
 			tseitinEncoding = new Encoding();
 			tseitinEncoding.encode(tree, implType);
-			return new DimacsCNF(
-					tseitinEncoding.getClauses(), tree.getNNFVariables(), tseitinEncoding.getTseitinVariables());
+			return new DimacsCNF(tseitinEncoding.getClauses(), tree.getNNFVariables(),
+					tseitinEncoding.getTseitinVariables());
 		}
 		System.out.println(NOT_NNF);
 		return null;
